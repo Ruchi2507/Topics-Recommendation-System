6c533024d15a018557708a20893c9327bffff51a
Ruchika Chhabra <ruchika.chhabra25@gmail.com>
2019-10-23 22:43:49 +0530

    Some optimizations done
    - Word2Vec model is trained only on tags_set, as it contains list of tags assigned by artists for different artworks.
       Tags assigned for an artwork are considered as related.
    - Topics/tags related to given topic is predicted by using word2vec model's most similar words instead of computing similarity score with each word in dataset and then returning top 10 most-similar words.

--- topicRecommenderSystem.py
+++ topicRecommenderSystem.py
@@ -1,11 +1,29 @@
+"""
+topicRecommenderSystem.py
+It is responsible for finding the words/phrases related to initial query i.e. word/phrase
+"""
 import pandas as pd
 import gensim
 import pickle
 
+#Trained model is saved as pickle file
 trainedModelFileName = 'trainedModels/word2vec_model.pickle'
-knowledgeBaseFileName = 'trainedModels/knowledgeBase.pickle'
 
 def readAndPreprocessDataSet():
+    """
+    DESCRIPTION:
+    ------------
+    This API is responsible for:
+    - reading the dataset and converting the data to list of lists
+      so that inner list represents a sentence consisting of words/phrases.
+    - perform text pre-processing i.e. converting text to lowercase
+    
+    RETURN:
+    -------
+    Returns the list of list of tags where inner list represents different 
+    tags (i.e. words/phrases) assigned by artist for an artwork and are
+    considered are related.
+    """
     df = pd.read_csv("dataset/tag_sets.csv", header=None, delimiter=",")
     df.head()
     
@@ -15,72 +33,67 @@ def readAndPreprocessDataSet():
     del df
     
     #Create list of list for the data read from tags_set
-    #Inside list represents each row of csv file.
-    tagsList = [str(tag[0]).split(',') for tag in tagsList]
+    #Words are also converted to lowercase
+    tagsList = [list(map(lambda x: x.lower(), str(tag[0]).split(','))) for tag in tagsList]
     
-    #TODO: Convert words to lowercase
-    
-    #Reading the user keywords as well
-    df2 = pd.read_csv("dataset/user_keywords.tsv", delimiter='\t')
-    keywordsList = df2["keywords"].values.tolist()
-    keywordsList = [keywords.split(',') for keywords in keywordsList]
-    del df2
-    
-    knowledgeBase = tagsList #+ keywordsList
-    return knowledgeBase
+    return tagsList
 
 def trainAndStoreWord2VecModel(knowledgeBase):    
-    #Train Word2Vec model with the words in tag_set and user_keywords
+    """
+    DESCRIPTION:
+    -----------
+    Trains the Word2Vec model with the dataset received as argument.
+    
+    PARAMETERS:
+    -----------
+    knowledgeBase: list of lists. Inner list represents words/phrases related to an artwork.
+    """
+    #Train Word2Vec model with the words in tag_set 
     #For Word2Vec Training input format should be list of lists
-    model = gensim.models.Word2Vec(knowledgeBase, min_count=1,size=300,workers=4)
+    model = gensim.models.Word2Vec(knowledgeBase, min_count=1,size=300,workers=4,iter=5)
     #save the trained model to disk
     with open(trainedModelFileName, "wb") as f:
         pickle.dump(model, f)
 
-def createAndStoreFlattenedList(knowledgeBase):    
-    #Flatten the knowledgeBase to get list of words/phrases in tag_set and user_keywords
-    flatList = []
-    for subList in knowledgeBase:
-        for item in subList:
-            flatList.append(item)
-    knowledgeBase = flatList
-    del flatList
-    
-    #Remove duplicacy from knowledgeBase to get list of unique words/phrases
-    knowledgeBase = list(set(knowledgeBase))
-    #save the flattened knowledgeBase (list of words/phrases) to disk
-    #to be used for testing
-    with open(knowledgeBaseFileName, "wb") as f:
-        pickle.dump(knowledgeBase, f)
-
-#for the given word find top 10 similar words from knowledgeBase
-#if no similar word found in dictionary then empty JSON is returned.
 def searchRelatedTopics(givenTopic=""):
+    """
+    DESCRIPTION:
+    ------------
+    - This API uses the trained model to predict the words/phrases which are
+      related to given topic(i.e. word/phrase).
+    - if no similar topic is found then empty dict is returned.
+    
+    PARAMETER:
+    ----------
+    Topic(i.e. word/phrase) for which related topics or tags(i.e words/phrases)
+    need to be predicted.
+    
+    RETURN:
+    ------
+    Dict with key: related_topics and value as list of related topics
+    """
     givenTopic = givenTopic.replace('+',' ')
     
     with open(trainedModelFileName, "rb") as f:
         model = pickle.load(f)
     
-    with open(knowledgeBaseFileName, "rb") as f:
-        knowledgeBase = pickle.load(f)
-        
     relatedTopics={}
-    topicSimilarityDict={}
+    relatedTopicsList=[]
     if givenTopic and givenTopic in model.wv.vocab:
-        for topic in knowledgeBase:
-            topicSimilarityDict[topic]= model.wv.similarity(givenTopic, topic)
-    #Sort the topicSimilarityDict by values
-    topicSimilarityDict = dict(sorted(topicSimilarityDict.items(), key=lambda item: item[1],reverse=True))
-    #Get top 10 tags related to givenTopic
-    relatedTopics["related_topics"] = [*topicSimilarityDict.keys()][:10]
-    
+        relatedTopicsList = list( map(lambda x: x[0],model.wv.most_similar(givenTopic)))    
+    relatedTopics["related_topics"]=relatedTopicsList
     return relatedTopics
 
 if __name__ == '__main__':
+    """
+    When this file is executed independently then:
+    - it reads and pre-process the dataset i.e. tags assigned to different artwork
+    - Train Word2Vec model and save the trained model as pickle file
+    - Execute a sample test case
+    """
     print("Reading the Data Set")
     dataSet = readAndPreprocessDataSet()
     print("Training and Saving Word2Vec Model")
     trainAndStoreWord2VecModel(dataSet)    
-    createAndStoreFlattenedList(dataSet)
     print("Test the saved Model")
-    searchRelatedTopics("cat")
+    print(searchRelatedTopics("cat"))
